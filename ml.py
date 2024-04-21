3d8fb3140b6fac14aa4a023be8ce3cffd090a5f4
gitggjhub <jgaurgopal@gmail.com>
2024-04-21 07:57:19 +0530

    display

--- ml.py
+++ ml.py
@@ -42,7 +42,7 @@ prompt = [
 
 # Streamlit App
 st.set_page_config(page_title="I can Retrieve Any SQL query")
-st.header("Gemini App To Retrieve SQL Data")
+st.header("Gemini App To Retrieve Customer Data")
 
 question = st.text_input("Input:")
 submit = st.button("Ask the question")
